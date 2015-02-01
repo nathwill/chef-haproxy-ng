c724b651ab6cf6718994706943b8b1f5cae660e1
Nathan Williams <nathan@teamtreehouse.com>
2015-01-31 22:00:49 -0800

    stub helpers

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,15 +1,33 @@
 module Haproxy
   module Helpers
-    def self.config_blob(config = {})
-      blog = []
+    def self.instances
+      self.resource(Chef::Resource::HaproxyInstance)
+    end
 
-      config.each_pair do |param, val|
-        Array(val).each do |v|
-          blob << "#{param} #{v}"
-        end
+    def self.instance(name)
+      self.instances.select { |i| i.name == name }
+    end
+
+    def self.instance_config(instance)
+      
+    end
+
+    def self.proxies
+      self.resource(Chef::Resource::HaproxyProxy)
+    end
+
+    def self.proxy(name)
+      self.proxies.select { |p| p.name == name }
+    end
+
+    def self.proxy_config
+      
+    end
+
+    private def self.resource(resource)
+      run_context.resource_collection.select do |r|
+        r.is_a?(resource)
       end
-
-      blob.join("\n")
     end
   end
 end
