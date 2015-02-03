209c7175d8c657572504ac120030c0d183dd2606
Nathan Williams <nathan@teamtreehouse.com>
2015-02-02 22:54:11 -0800

    test stubs

---
+++ test/integration/source/serverspec/default_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'haproxy-ng::default' do
+
+  # Serverspec examples can be found at
+  # http://serverspec.org/resource_types.html
+  
+  it 'does something' do
+    skip 'Replace this with meaningful tests'
+  end
+
+end
