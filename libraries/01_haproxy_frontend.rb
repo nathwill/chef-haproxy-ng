8d634925b100e38effd6d5050f1d928dd74a4182
Nathan Williams <nathan@teamtreehouse.com>
2015-02-04 19:24:03 -0800

    re-org for clarity

--- libraries/01_haproxy_frontend.rb
+++ libraries/01_haproxy_frontend.rb
@@ -30,29 +30,34 @@ class Chef::Provider
       super
     end
 
-    # rubocop: disable AbcSize
-    # rubocop: disable MethodLength
     def load_current_resource
       @current_resource ||= Chef::Resource::HaproxyFrontend.new(
         new_resource.name
       )
       @current_resource.type new_resource.type
-      merged_config = new_resource.config
-      merged_config.unshift("mode #{new_resource.mode}") if new_resource.mode
+      @current_resource.config merged_config(new_resource.config)
+      @current_resource
+    end
+
+    private
+
+    # rubocop: disable AbcSize
+    # rubocop: disable MethodLength
+    def merged_config(config)
+      config.unshift("mode #{new_resource.mode}") if new_resource.mode
       Array(new_resource.bind).each do |bind|
-        merged_config.unshift("bind #{bind}")
+        config.unshift("bind #{bind}")
       end
       new_resource.acls.each do |acl|
-        merged_config << "acl #{acl[:name]} #{acl[:criterion]}"
+        config << "acl #{acl[:name]} #{acl[:criterion]}"
       end
       new_resource.use_backends.each do |ub|
-        merged_config << "use_backend #{ub[:backend]} #{ub[:condition]}"
+        config << "use_backend #{ub[:backend]} #{ub[:condition]}"
       end
       if new_resource.default_backend
-        merged_config << "default_backend #{new_resource.default_backend}"
+        config << "default_backend #{new_resource.default_backend}"
       end
-      @current_resource.config merged_config
-      @current_resource
+      config
     end
     # rubocop: enable AbcSize
     # rubocop: enable MethodLength
