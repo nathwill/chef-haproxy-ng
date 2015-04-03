ddaf1d3a8d0529b219d6970fcfcc2be801781690
Nathan Williams <nathan@teamtreehouse.com>
2015-04-03 01:25:00 -0700

    handle nil input

--- libraries/00_helpers.rb
+++ libraries/00_helpers.rb
@@ -429,7 +429,7 @@ module Haproxy
       # rubocop: disable MethodLength
       def servers(arg = nil)
         set_or_return(
-          :servers, arg.sort_by { |s| s['name'] },
+          :servers, arg ? arg.sort_by { |s| s['name'] } : arg,
           :kind_of => Array,
           :default => [],
           :callbacks => {
