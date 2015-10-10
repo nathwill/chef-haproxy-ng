2fafc71c628e33260d94438e50edf9b89fc77705
Nathan Williams <nathan@teamtreehouse.com>
2015-10-09 17:01:33 -0700

    fix testing consul use

--- test/fixtures/cookbooks/my-consul-lb/recipes/consul.rb
+++ test/fixtures/cookbooks/my-consul-lb/recipes/consul.rb
@@ -1,16 +1,19 @@
 include_recipe 'consul'
 
-consul_service_def 'haproxy-peers' do
-  port 1024
-  notifies :reload, 'service[consul]'
+consul_definition 'haproxy-peers' do
+  type 'service'
+  parameters port: 1024
+  notifies :reload, 'consul_service[consul]'
 end
 
-consul_service_def 'mysql' do
-  port 3306
-  notifies :reload, 'service[consul]'
+consul_definition 'mysql' do
+  type 'service'
+  parameters port: 3306
+  notifies :reload, 'consul_service[consul]'
 end
 
-consul_service_def 'my-app' do
-  port 8080
-  notifies :reload, 'service[consul]'
+consul_definition 'my-app' do
+  type 'service'
+  parameters port: 8080
+  notifies :reload, 'consul_service[consul]'
 end
